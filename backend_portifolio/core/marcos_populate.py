d72622e9e6f97c83c57b2b3f1532dbab55dc09d1
mlobf <marcoslemeborbafilho@gmail.com>
2021-08-29 14:16:15 -0300

    Solved DataTime Issue

--- backend_portifolio/core/marcos_populate.py
+++ backend_portifolio/core/marcos_populate.py
@@ -30,51 +30,57 @@ RentLCY = 9
 UtilitiesLCY = 10
 FacilityLCY = 11
 SuppliesLCY = 12
+'''
+entry = {
 
+    'Region': x[Region],
+    'BuidingId': x[BuidingId],
+    'CostCentre': x[CostCentre],
+    'TelcoCode':  x[TelcoCode],
+    'BuildingCurrentUse': x[BuildingCurrentUse],
+    'Country': x[Country],
+    'LocalCurrencyName': x[LocalCurrencyName],
+    'CostDate': x[CostDate],
+    'BudFXRate': x[BudFXRate],
+    'RentLCY':  x[RentLCY],
+    'UtilitiesLCY': x[UtilitiesLCY],
+    'FacilityLCY': x[FacilityLCY],
+    'SuppliesLCY': x[SuppliesLCY],
+
+}
+'''
 
 lista = []
+
+
 for x in df['data']:
-    print('       ------------------------     ')
-    print((x[Region]))
-    print((x[BuidingId]))
-    print((x[CostCentre]))
-    print((x[TelcoCode]))
-    print((x[BuildingCurrentUse]))
-    print((x[Country]))
-    print((x[LocalCurrencyName]))
-    print((x[CostDate]))
-    print((x[BudFXRate]))
-    print((x[RentLCY]))
-    print((x[UtilitiesLCY]))
-    print((x[FacilityLCY]))
-    print((x[SuppliesLCY]))
+
+    entry = {
+
+        'Region': x[Region],
+        'BuidingId': x[BuidingId],
+        'CostCentre': x[CostCentre],
+        'TelcoCode':  x[TelcoCode],
+        'BuildingCurrentUse': x[BuildingCurrentUse],
+        'Country': x[Country],
+        'LocalCurrencyName': x[LocalCurrencyName],
+        'CostDate': str(x[CostDate]),
+        'BudFXRate': x[BudFXRate],
+        'RentLCY':  x[RentLCY],
+        'UtilitiesLCY': x[UtilitiesLCY],
+        'FacilityLCY': x[FacilityLCY],
+        'SuppliesLCY': x[SuppliesLCY],
+    }
+    lista.append(entry)
+
+print(lista[0])
+
+json_lists = json.dumps(lista)
+print(json_lists)
 
 
 # xls.data = df
 # xls.save()
 
-
 # TypeError: Object of type DataFrame is not JSON serializable
 # df_json = json.dumps(df)
-
-
-'''
-count = 0
-for _ in df:
-    print(
-        df[count]["Region"],
-        df[count]["BuidingId"],
-        df[count]["CostCentre"],
-        df[count]["TelcoCode"],
-        df[count]["BuildingCurrentUse"],
-        df[count]["Country"],
-        df[count]["LocalCurrencyName(LCY)"],
-        df[count]["CostDate"],
-        df[count]["2010BudFXRate"],
-        df[count]["RentLCY"],
-        df[count]["UtilitiesLCY"],
-        df[count]["FacilityLCY"],
-        df[count]["SuppliesLCY"],
-    )
-    count = count + 1
-'''
