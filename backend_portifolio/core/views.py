bce59d4b3c52d0b43421c553b97a51a7e9cc32f0
mlobf <marcoslemeborbafilho@gmail.com>
2021-08-29 16:49:00 -0300

    Details list Done

--- backend_portifolio/core/views.py
+++ backend_portifolio/core/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from core.models import Planilha
+import pandas as pd
 
 # Create your views here.
 
@@ -31,4 +32,48 @@ def list_all_xls(request):
 
 
 def list_details_xls(request, id):
-    pass
+
+    xls = Planilha.objects.get(id=id)
+    xls_file = xls.file
+    df = pd.DataFrame(pd.read_excel(xls_file, header=0, index_col=False))
+    df = df.to_dict("split")
+
+    Region = 0
+    BuidingId = 1
+    CostCentre = 2
+    TelcoCode = 3
+    BuildingCurrentUse = 4
+    Country = 5
+    LocalCurrencyName = 6
+    CostDate = 7
+    BudFXRate = 8
+    RentLCY = 9
+    UtilitiesLCY = 10
+    FacilityLCY = 11
+    SuppliesLCY = 12
+
+    lista = []
+    for x in df['data']:
+
+        entry = {
+            'Region': x[Region],
+            'BuidingId': x[BuidingId],
+            'CostCentre': x[CostCentre],
+            'TelcoCode':  x[TelcoCode],
+            'BuildingCurrentUse': x[BuildingCurrentUse],
+            'Country': x[Country],
+            'LocalCurrencyName': x[LocalCurrencyName],
+            'CostDate': str(x[CostDate]),
+            'BudFXRate': x[BudFXRate],
+            'RentLCY':  x[RentLCY],
+            'UtilitiesLCY': x[UtilitiesLCY],
+            'FacilityLCY': x[FacilityLCY],
+            'SuppliesLCY': x[SuppliesLCY],
+        }
+        lista.append(entry)
+
+    print(lista)
+
+    context = {'context': lista}
+
+    return render(request, "planilha/planilha_details.html", context)
