51d075ca2cea5a1533658c940149f8d97a226b9b
mlobf <marcoslemeborbafilho@gmail.com>
2021-08-29 21:51:22 -0300

    Add DRF List Planilhas

--- backend_portifolio/api/views.py
+++ backend_portifolio/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from core.models import Planilha
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.serializers import PlanilhaSerializer
 # Create your views here.
+
+
+class PlanihaListAPI(APIView):
+
+    def get(self, request):
+
+        planilha = Planilha.objects.all()
+        serializer = PlanilhaSerializer(planilha, many=True)
+
+        return Response(serializer.data)
